44c75a1e2b03aff594d21f1adfa8c0672ad5961d
Serj Kazar <serjkazar@gmail.com>
2013-04-25 20:35:11 -0400

    Clean up code, add touch capabilities.
    still work in progress:
    + added touch changing location in setup process
    - pressing button counts to touch - it shouldn't
    - 'set wallpaper' defaults to original position - not the one touch
    gives you

--- LoLLiveWallpaper/src/hyoma/app/lollivewallpaper/MyWallpaperService.java
+++ LoLLiveWallpaper/src/hyoma/app/lollivewallpaper/MyWallpaperService.java
@@ -1,11 +1,7 @@
 package hyoma.app.lollivewallpaper;
 
 
-import java.util.ArrayList;
-import java.util.List;
 
-import android.R.color;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,8 +13,7 @@ import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import hyoma.app.lollivewallpaper.MyApplication;
-
+import hyoma.app.lollivewallpaper.SetWallpaperActivity;
 public class MyWallpaperService extends WallpaperService {
 
 	@Override
@@ -26,7 +21,9 @@ public class MyWallpaperService extends WallpaperService {
 		return new MyWallpaperEngine();
 	}
 
-	private class MyWallpaperEngine extends Engine {
+	private class MyWallpaperEngine extends Engine { 
+		float lastX = SetWallpaperActivity.Measuredwidth/2;
+		float lastY = SetWallpaperActivity.Measuredheight/2;
 		private int animationCount = 0;
 		private final Handler handler = new Handler();
 		private final Runnable drawRunner = new Runnable() {
@@ -34,21 +31,14 @@ public class MyWallpaperService extends WallpaperService {
 			public void run() {
 				draw();
 			}
-
 		};
-		private List<MyPoint> circles;
 		private Paint paint = new Paint();
-		private int width;
-		int height;
 		private boolean visible = true;
-		private int maxNumber;
 		private boolean touchEnabled;
 
 		public MyWallpaperEngine() {
 			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MyWallpaperService.this);
-			maxNumber = Integer.valueOf(prefs.getString("numberOfCircles", "4"));
-			touchEnabled = prefs.getBoolean("touch", false);
-			circles = new ArrayList<MyPoint>();
+			touchEnabled = prefs.getBoolean("touch", true);
 			paint.setAntiAlias(true);
 			paint.setColor(Color.WHITE);
 			paint.setStyle(Paint.Style.STROKE);
@@ -82,10 +72,9 @@ public class MyWallpaperService extends WallpaperService {
 
 		// Called immediately after any structural change. Always called at least once after creation.
 		@Override
-		public void onSurfaceChanged(SurfaceHolder holder, int format,
-				int width, int height) {
-			this.width = width;
-			this.height = height;
+		public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+			this.lastX = width;
+			this.lastY = height;
 			super.onSurfaceChanged(holder, format, width, height);
 		}
 
@@ -96,28 +85,43 @@ public class MyWallpaperService extends WallpaperService {
 		public void onTouchEvent(MotionEvent event) {
 			if (touchEnabled) {
 
-				float x = event.getX();
-				float y = event.getY();
+				lastX = event.getX();
+				lastY = event.getY();
 				SurfaceHolder holder = getSurfaceHolder();
 				Canvas canvas = null;
-				try {
-					canvas = holder.lockCanvas();
-					if (canvas != null) {
-						canvas.drawColor(Color.BLACK);
-						circles.clear();
-						circles.add(new MyPoint(String.valueOf(circles.size() + 1), x, y));
-						drawCircles(canvas, circles);
-					}
-				} finally {
-					if (canvas != null)
-						holder.unlockCanvasAndPost(canvas);
+				
+				BitmapFactory bm = new BitmapFactory(); 
+				String animFrame = "chibimord_frame" + animationCount; 
+				animationCount++;
+				if(animationCount > 39){
+					animationCount = 0;
 				}
-				super.onTouchEvent(event);
+				int identifier = 0;
+				identifier = getResources().getIdentifier(animFrame,"drawable", "hyoma.app.lollivewallpaper");
+				if(identifier != 0){
+					Bitmap img = bm.decodeResource(getResources(), identifier);  					  	
+					try {
+						// lockCanvas():
+						// Start editing the pixels in the surface. The returned Canvas can be used to draw into the surface's bitmap. 
+						// A null is returned if the surface has not been created or otherwise cannot be edited. 
+						// You will usually need to implement Callback.surfaceCreated to find out when the Surface is available for use.
+						canvas = holder.lockCanvas();
+						if (canvas != null) {
+							canvas.drawColor(Color.BLACK);
+							canvas.drawBitmap(img,lastX,lastY, null);
+						}
+					} finally {
+						if (canvas != null)
+							holder.unlockCanvasAndPost(canvas);
+					}
+				}
 			}
+		super.onTouchEvent(event);
 		}
 
 		// The main look of the wallpaper. 
 		private void draw() {
+
 			// This holder holds the image and allows one to change the pixels. 
 			SurfaceHolder holder = getSurfaceHolder();
 			Canvas canvas = null;
@@ -131,14 +135,7 @@ public class MyWallpaperService extends WallpaperService {
 			int identifier = 0;
 			identifier = getResources().getIdentifier(animFrame,"drawable", "hyoma.app.lollivewallpaper");
 			if(identifier != 0){
-				Bitmap img = bm.decodeResource(getResources(), identifier);
-				
-				
-				//AnimationDrawable chibiMordAnimation;
-				//ImageView chibiMord = (ImageView) findViewById(R.id.chibi_mord);
-				//chibiMord.setBackgroundResource(R.drawable.chibimord);
-				//chibiMordAnimation = (AnimationDrawable) chibiMord.getBackground();
-				  
+				Bitmap img = bm.decodeResource(getResources(), identifier);				  
 				try {
 					// lockCanvas():
 					// Start editing the pixels in the surface. The returned Canvas can be used to draw into the surface's bitmap. 
@@ -147,16 +144,7 @@ public class MyWallpaperService extends WallpaperService {
 					canvas = holder.lockCanvas();
 					if (canvas != null) {
 						canvas.drawColor(Color.BLACK);
-						canvas.drawBitmap(img,200,200, null);
-						/*
-						if (circles.size() >= maxNumber) {
-							circles.clear();
-						}
-						int x = (int) (width * Math.random());
-						int y = (int) (height * Math.random());
-						circles.add(new MyPoint(String.valueOf(circles.size() + 1), x, y));
-						drawCircles(canvas, circles);
-						*/
+						canvas.drawBitmap(img,lastX,lastY, null);
 					}
 				} finally {
 					if (canvas != null)
@@ -168,14 +156,5 @@ public class MyWallpaperService extends WallpaperService {
 				handler.postDelayed(drawRunner, 20);
 			}
 		}
-
-		// Surface view requires that all elements are drawn completely
-		private void drawCircles(Canvas canvas, List<MyPoint> circles) {
-			canvas.drawColor(Color.BLACK);
-			for (MyPoint point : circles) {
-				canvas.drawCircle(point.x, point.y, 20.0f, paint);
-			}
-		}
 	}
-
 }
--- LoLLiveWallpaper/src/hyoma/app/lollivewallpaper/SetWallpaperActivity.java
+++ LoLLiveWallpaper/src/hyoma/app/lollivewallpaper/SetWallpaperActivity.java
@@ -1,20 +1,43 @@
 package hyoma.app.lollivewallpaper;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class SetWallpaperActivity extends Activity {
+	static float Measuredwidth = 0;
+	static float Measuredheight = 0;
+	
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		Point size = new Point();
+		WindowManager w = getWindowManager();
+	    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2){
+          w.getDefaultDisplay().getSize(size);
+
+          Measuredwidth = size.x;
+          Measuredheight = size.y; 
+        }else{
+          Display d = w.getDefaultDisplay(); 
+          Measuredwidth = d.getWidth(); 
+          Measuredheight = d.getHeight(); 
+        }		
+		
 		super.onCreate(savedInstanceState); // This activity itself creates the light grey atmosphere. Must be a property of the activity theme. 
 		setContentView(R.layout.main); // the main.xml in res/layout/ is what will set any menu/buttons on start of app.
 	}
 
-	public void onClick(View view) {
+	public void onButton1Click(View view) {
 		// This is called when button is clicked. It does all the work of creating the background. 
 		Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
 		intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(this, MyWallpaperService.class));
